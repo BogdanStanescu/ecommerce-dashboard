92e6ef21385dd3421f7afa6b78dd038838e4af64
Stanescu Bogdan <bogdan.stanescu96@gmail.com>
2023-11-07 19:28:19 +0200

    feat: implement modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,18 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import AuthLayout from "../(auth)/layout";
+"use client";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
-  return (
-    <nav className="p-4">
-      <UserButton afterSignOutUrl="/" />
-    </nav>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root page</div>;
 };
 
 export default SetupPage;
---
+++ components/ui/modal.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+
+interface IModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: ReactNode;
+}
+
+export const Modal = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}: IModalProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={() => !isOpen && onClose()}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+
+        <>{children}</>
+      </DialogContent>
+    </Dialog>
+  );
+};
