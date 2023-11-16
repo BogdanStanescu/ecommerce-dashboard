578983da725682af724c2a701aad5c20a0408ae7
Stanescu Bogdan <bogdan.stanescu96@gmail.com>
2023-11-16 21:05:37 +0200

    docs: update website description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,14 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToastProvider } from "@/providers/toast-provider";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Admin Dashboard",
-  description: "Admin Dashboard",
+  title: "E-Commerce Admin Dashboard",
+  description:
+    "Boost your e-commerce control with our sleek admin dashboard. Effortlessly manage orders, track performance, and elevate your business. Simplify, streamline, succeed.",
 };
 
 export default function RootLayout({
@@ -26,6 +28,7 @@ export default function RootLayout({
           <ModalProvider />
 
           {children}
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
