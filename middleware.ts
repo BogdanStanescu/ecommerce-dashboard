fc08759756f84f3d0ef00b3ed64dd95495ecbaa2
Stanescu Bogdan <bogdan.stanescu96@gmail.com>
2023-12-09 20:23:29 +0200

    chore: add Stripe as payment processor

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,9 @@ export default authMiddleware({
     "/api/a3de4c05-3ca9-4e42-bb9d-3c2e5ef3f698/billboards/c116c37a-3727-462a-952b-1e7bfa6331b5",
     "/api/a3de4c05-3ca9-4e42-bb9d-3c2e5ef3f698/products",
     "/api/a3de4c05-3ca9-4e42-bb9d-3c2e5ef3f698/products/6971c893-4b83-4470-ab53-3c913c6c233a",
+    "/api/a3de4c05-3ca9-4e42-bb9d-3c2e5ef3f698/categories/4842bab0-3036-445f-821c-9203fd811a69",
+    "/api/a3de4c05-3ca9-4e42-bb9d-3c2e5ef3f698/colors",
+    "/api/a3de4c05-3ca9-4e42-bb9d-3c2e5ef3f698/sizes",
   ],
 });
 
